5c66d7bfd1f3f67e2a1cc797cf578667371c8a59
Kanchana Santhanakrishnan <ksanthanakrishnan@g.harvard.edu>
2014-09-28 22:31:33 -0400

    Corrections to the validator

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ ini_set('display_errors', 1);
 <img src="http://imgs.xkcd.com/comics/password_strength.png" alt="xcd password generator">
 
 <p>
-    <a href="http://jigsaw.w3.org/css-validator/validator?uri=p2.kanch.me&ampprofile=css3&ampusermedium=all&ampwarning=1&ampvextwarning=&amplang=en">
+    <a href="http://jigsaw.w3.org/css-validator/validator?uri=p2.kanch.me&amp;profile=css3&amp;usermedium=all&amp;warning=1&amp;vextwarning=&amp;lang=en">
     Validate CSS
     </a>
     <br>
